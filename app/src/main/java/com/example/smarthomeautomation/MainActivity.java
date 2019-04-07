99d983cdb7db283aebdbe852becd383e3b1af868
kishansinghverma <kishansinghverma@gmail.com>
2019-04-08 00:01:34 +0530

    init commit

---
+++ app/src/main/java/com/example/smarthomeautomation/MainActivity.java
@@ -0,0 +1,188 @@
+package com.example.smarthomeautomation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button update;
+    Switch fansw;
+    Switch bulbsw;
+    TextView temptv;
+    TextView fantv;
+    TextView bulbtv;
+
+    ImageView faniv;
+    ImageView bulbiv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        update=findViewById(R.id.btntmp);
+        fansw=findViewById(R.id.btnfan);
+        bulbsw=findViewById(R.id.btnbulb);
+        temptv =findViewById(R.id.tvtemp);
+        fantv=findViewById(R.id.tvfan);
+        bulbtv=findViewById(R.id.tvbulb);
+        faniv=findViewById(R.id.ifan);
+        bulbiv=findViewById(R.id.ibulb);
+
+        doRequest("get");
+
+        faniv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(fansw.isChecked())
+                    fansw.setChecked(false);
+                else
+                    fansw.setChecked(true);
+                switchHandler(null);
+            }
+        });
+
+        bulbiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bulbsw.isChecked())
+                    bulbsw.setChecked(false);
+                else
+                    bulbsw.setChecked(true);
+                switchHandler(null);
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doRequest("get");
+            }
+        });
+
+    }
+
+    public void switchHandler(View v)
+    {
+        doRequest("post");
+        try {
+            TimeUnit.MILLISECONDS.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        doRequest("get");
+    }
+
+    public void doRequest(String operation){
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        if(operation.equals("get")) {
+            String url = "http://kloud.cf/getStatus.php";
+            StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response) {
+                            String[] status=response.split("/");
+                            float tmp=Float.parseFloat(status[2]);
+                            temptv.setText(String.valueOf(tmp)+" C\nLast Updated\n"+status[3]);
+
+                            if(Float.parseFloat(status[0])==1){
+                                fantv.setText("Fan Is On!");
+                                fantv.setTextColor(getResources().getColor(R.color.colorskyblue));
+                                fansw.setChecked(true);
+                            }
+                            else
+                            {
+                                fantv.setText("Fan Is Off!");
+                                fantv.setTextColor(getResources().getColor(R.color.colorred));
+                                fansw.setChecked(false);
+                            }
+                            if(Float.parseFloat(status[1])==1){
+                                bulbtv.setText("Bulb Is On!");
+                                bulbtv.setTextColor(getResources().getColor(R.color.colorskyblue));
+                                bulbsw.setChecked(true);
+                            }
+                            else
+                            {
+                                bulbtv.setText("Bulb Is Off!");
+                                bulbtv.setTextColor(getResources().getColor(R.color.colorred));
+                                bulbsw.setChecked(false);
+                            }
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // error
+                            //Log.d("Error.Response", response);
+                        }
+                    }
+            ) {
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String>  params = new HashMap<String, String>();
+                    return params;
+                }
+            };
+            queue.add(postRequest);
+        }
+
+        else
+        {
+            String url = "http://kloud.cf/postStatus.php";
+            StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response) {
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // error
+                            //Log.d("Error.Response", response);
+                        }
+                    }
+            ) {
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String>  params = new HashMap<String, String>();
+                    if(fansw.isChecked())
+                        params.put("fan", "1");
+                    else
+                        params.put("fan", "0");
+                    if(bulbsw.isChecked())
+                        params.put("bulb","1");
+                    else
+                        params.put("bulb", "0");
+
+                    return params;
+                }
+            };
+            queue.add(postRequest);
+        }
+    }
+}
