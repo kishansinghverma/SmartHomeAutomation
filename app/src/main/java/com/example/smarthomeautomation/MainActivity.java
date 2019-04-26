153ad6c01dee409a4c445ca77813c3ffcdc5c2de
kishansinghverma <kishansinghverma@gmail.com>
2019-04-27 01:16:35 +0530

    commit

--- app/src/main/java/com/example/smarthomeautomation/MainActivity.java
+++ app/src/main/java/com/example/smarthomeautomation/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.smarthomeautomation;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,6 +27,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +39,6 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button update;
     Switch fansw;
     Switch bulbsw;
     TextView temptv;
@@ -31,12 +48,18 @@ public class MainActivity extends AppCompatActivity {
     ImageView faniv;
     ImageView bulbiv;
 
+    ProgressBar progressBar;
+    LinearLayout linearLayout;
+
+    FirebaseDatabase database;
+    Button update;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        update=findViewById(R.id.btntmp);
+        update=findViewById(R.id.update);
         fansw=findViewById(R.id.btnfan);
         bulbsw=findViewById(R.id.btnbulb);
         temptv =findViewById(R.id.tvtemp);
@@ -44,8 +67,34 @@ public class MainActivity extends AppCompatActivity {
         bulbtv=findViewById(R.id.tvbulb);
         faniv=findViewById(R.id.ifan);
         bulbiv=findViewById(R.id.ibulb);
+        progressBar=findViewById(R.id.progressBar);
+        linearLayout=findViewById(R.id.linearLayout);
 
-        doGet();
+        initialize();
+
+        database=FirebaseDatabase.getInstance();
+        DatabaseReference smoke=database.getReference("smoke");
+        smoke.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    makeNotificationChannel("CHANNEL", "Priority Channel", NotificationManager.IMPORTANCE_DEFAULT);
+                }
+
+                NotificationCompat.Builder notification = new NotificationCompat.Builder(MainActivity.this, "CHANNEL_1");
+                notification.setSmallIcon(R.mipmap.ic_launcher);
+                notification.setContentTitle("Alert!");
+                notification.setContentText("Smoke Detected In Your House!");
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(1, notification.build());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         faniv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                     fansw.setChecked(false);
                 else
                     fansw.setChecked(true);
-                doPost(v);
+                fanUpdate(v);
             }
         });
 
@@ -65,48 +114,37 @@ public class MainActivity extends AppCompatActivity {
                     bulbsw.setChecked(false);
                 else
                     bulbsw.setChecked(true);
-                doPost(v);
+                bulbUpdate(v);
             }
         });
 
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doGet();
+                progressBar.setVisibility(View.VISIBLE);
+                updateDHT();
             }
         });
 
     }
 
-    public void doGet()
-    {
+    private void updateDHT() {
         RequestQueue getQueue = Volley.newRequestQueue(this);
-        String url = "http://kloud.cf/getStatus.php";
+        String url = "https://erratic-porcupine-6148.dataplicity.io/cgi-bin/get_temp.py";
         StringRequest getRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        String[] status = response.split("/");
-                        float tmp = Float.parseFloat(status[2]);
-                        temptv.setText(String.valueOf(tmp) + " C\nLast Updated\n" + status[3]);
-
-                        if (Float.parseFloat(status[0]) == 1) {
-                            fantv.setText("Fan Is On!");
-                            fantv.setTextColor(getResources().getColor(R.color.colorskyblue));
-                            fansw.setChecked(true);
-                        } else {
-                            fantv.setText("Fan Is Off!");
-                            fantv.setTextColor(getResources().getColor(R.color.colorred));
-                            fansw.setChecked(false);
+                        try {
+                            progressBar.setVisibility(View.GONE);
+                            String[] status = response.trim().split("/");
+                            float tmp = Float.parseFloat(status[0]);
+                            float humid = Float.parseFloat(status[1]);
+                            temptv.setText(String.valueOf(tmp) + "\u00B0C~" + humid + "%\nLast Updated\n" + status[2]);
                         }
-                        if (Float.parseFloat(status[1]) == 1) {
-                            bulbtv.setText("Bulb Is On!");
-                            bulbtv.setTextColor(getResources().getColor(R.color.colorskyblue));
-                            bulbsw.setChecked(true);
-                        } else {
-                            bulbtv.setText("Bulb Is Off!");
-                            bulbtv.setTextColor(getResources().getColor(R.color.colorred));
-                            bulbsw.setChecked(false);
+                        catch (Exception e)
+                        {
+                            Toast.makeText(MainActivity.this, "Connection Failed!!", Toast.LENGTH_SHORT).show();
                         }
                     }
                 },
@@ -118,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
                         //Log.d("Error.Response", response);
                     }
                 }
-                ) {
+        ) {
             @Override
             protected Map<String, String> getParams()
             {
@@ -129,17 +167,54 @@ public class MainActivity extends AppCompatActivity {
         getQueue.add(getRequest);
     }
 
-
-    public void doPost(View view) {
-        RequestQueue postQueue = Volley.newRequestQueue(this);
-        String url = "http://kloud.cf/postStatus.php";
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+    private void initialize() {
+        RequestQueue getQueue = Volley.newRequestQueue(this);
+        String url = "https://erratic-porcupine-6148.dataplicity.io/cgi-bin/get_data.py";
+        StringRequest getRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        try {
+                            String[] status = response.trim().split("/");
+                            float tmp = Float.parseFloat(status[2]);
+                            float humid = Float.parseFloat(status[3]);
+
+                            temptv.setText(String.valueOf(tmp) + "\u00B0C~" + humid + "%\nLast Updated\n" + status[4]);
+
+                            if (Float.parseFloat(status[0]) == 1) {
+                                fantv.setText("Fan Is On!");
+                                fantv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorskyblue));
+                                fansw.setChecked(true);
+                                faniv.setVisibility(View.GONE);
+                                (findViewById(R.id.gif)).setVisibility(View.VISIBLE);
+                            } else {
+                                fantv.setText("Fan Is Off!");
+                                fantv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorred));
+                                fansw.setChecked(false);
+                                (findViewById(R.id.gif)).setVisibility(View.GONE);
+                                faniv.setVisibility(View.VISIBLE);
+
+                            }
+                            if (Float.parseFloat(status[1]) == 1) {
+                                bulbtv.setText("Bulb Is On!");
+                                bulbtv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorskyblue));
+                                bulbsw.setChecked(true);
+                                bulbiv.setImageResource(R.drawable.bon);
+
+                            } else {
+                                bulbtv.setText("Bulb Is Off!");
+                                bulbtv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorred));
+                                bulbsw.setChecked(false);
+                                bulbiv.setImageResource(R.drawable.boff);
+                            }
+                        }
+                        catch (Exception e) {
+                            Toast.makeText(MainActivity.this, "Connection Failed!!", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 },
-                new Response.ErrorListener() {
+                new Response.ErrorListener()
+                {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // error
@@ -148,27 +223,114 @@ public class MainActivity extends AppCompatActivity {
                 }
         ) {
             @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                if (fansw.isChecked())
-                    params.put("fan", "1");
-                else
-                    params.put("fan", "0");
-                if (bulbsw.isChecked())
-                    params.put("bulb", "1");
-                else
-                    params.put("bulb", "0");
-
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
                 return params;
             }
         };
-        postQueue.add(postRequest);
-        postQueue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<StringRequest>() {
-            @Override
-            public void onRequestFinished(Request<StringRequest> request) {
-                doGet();
-            }
-        });
+        getQueue.add(getRequest);
+    }
 
+    public void fanUpdate(View v)
+    {
+        RequestQueue getQueue = Volley.newRequestQueue(this);
+        String url = "https://erratic-porcupine-6148.dataplicity.io/cgi-bin/fan_handler.py";
+        StringRequest getRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                     if(response.trim().equals("0"))
+                     {
+                         fantv.setText("Fan Is Off!");
+                         fantv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorred));
+                         fansw.setChecked(false);
+                         (findViewById(R.id.gif)).setVisibility(View.GONE);
+                         faniv.setVisibility(View.VISIBLE);
+                     }
+                     else if (response.trim().equals("1"))
+                     {
+                         fantv.setText("Fan Is On!");
+                         fantv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorskyblue));
+                         fansw.setChecked(true);
+                         faniv.setVisibility(View.GONE);
+                         (findViewById(R.id.gif)).setVisibility(View.VISIBLE);
+                     }
+                     else
+                         Toast.makeText(MainActivity.this, "Connection Failed!!", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String>  params = new HashMap<String, String>();
+                if(fansw.isChecked())
+                    params.put("fan", "1");
+                else
+                    params.put("fan", "0");
+                return params;
+            }
+        };
+        getQueue.add(getRequest);
+
+    }
+
+    public void bulbUpdate(View v)
+    {
+        RequestQueue getQueue = Volley.newRequestQueue(this);
+        String url = "https://erratic-porcupine-6148.dataplicity.io/cgi-bin/bulb_handler.py";
+        StringRequest getRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.trim().equals("0"))
+                        {
+                            bulbtv.setText("Bulb Is Off!");
+                            bulbtv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorred));
+                            bulbsw.setChecked(false);
+                            bulbiv.setImageResource(R.drawable.boff);
+                        }
+                        else if (response.trim().equals("1"))
+                        {
+                            bulbtv.setText("Bulb Is On!");
+                            bulbtv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorskyblue));
+                            bulbsw.setChecked(true);
+                            bulbiv.setImageResource(R.drawable.bon);
+                        }
+                        else
+                            Toast.makeText(MainActivity.this, "Connection Failed!!", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String>  params = new HashMap<String, String>();
+                if(bulbsw.isChecked())
+                    params.put("bulb", "1");
+                else
+                    params.put("bulb", "0");
+                return params;
+            }
+        };
+        getQueue.add(getRequest);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void makeNotificationChannel(String id, String name, int importance)
+    {
+        NotificationChannel channel = new NotificationChannel(id, name, importance);
+        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(channel);
     }
 }
